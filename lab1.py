028dc0330f203ce2f7b225fe527e2aed6ed413b4
Tasneem Adel <tasneem.adel8@gmail.com>
2026-03-05 14:11:04 +0200

    finish

--- lab1.py
+++ lab1.py
@@ -2,9 +2,13 @@
 def print_welcome(username):
     print(f"welcome {username}")
 
-
+def addition(num1,num2):
+    return num1 + num2
 
 
 username = "Tasneem Adel"
 print_welcome(username)
+addition(10,20)
 
+lab Done
+finish
